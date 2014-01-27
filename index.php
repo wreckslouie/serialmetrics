06734ed335e36662921032f9b67743e28f14e0f8
Bjorn Cooley <bjorncooley@gmail.com>
2014-01-27 15:18:41 -0500

    Update map to remove scrolling, add marker

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
     </script>
     <script type="text/javascript">
       function initialize() {
-
+      	var SMLatLong= new google.maps.LatLng(37.794508,-122.39493);
         var mapOptions = {
           center: new google.maps.LatLng(37.794508,-122.39493),
           zoom: 14
@@ -26,9 +26,17 @@
 	            ]
            }
         ];
-        var map = new google.maps.Map(document.getElementById("map-canvas"),
-            mapOptions);
-        map.setOptions({styles: styles});
+        var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+        map.setOptions({
+        	styles: styles,
+        	scrollwheel: false
+        });
+
+        var marker=new google.maps.Marker({
+             position:SMLatLong,
+             map: map
+
+        });
 
       }
       google.maps.event.addDomListener(window, 'load', initialize);
@@ -100,10 +108,7 @@
 	</div>
 
 	<!-- Google Map Embed Code -->
-	<iframe width="1680" height="400" frameborder="0" scrolling="no" marginheight="0" 
-	marginwidth="0" style="background-color:rgb(229,227,223);"
-	src="https://maps.google.com/maps/ms?msa=0&amp;msid=203123961827304078823.0004f07e9f20c4403cc04&amp;
-	ie=UTF8&amp;t=h&amp;ll=37.794322,-122.381859&amp;spn=0.023739,0.144196&amp;z=14&amp;output=embed"></iframe>
+	<div id="map-canvas"></div>
 
 	<footer>
 		<p>donec &bull; sapien &bull; erat &bull; lorem &bull; ipsum</p>
