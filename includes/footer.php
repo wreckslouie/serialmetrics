05d2066cd8f3155ec48954a1d21cc455234561e2
Bjorn Cooley <bjorncooley@gmail.com>
2014-02-25 08:41:11 -0800

    Update footer with live links, dark text an white inside pages

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,6 @@
 <footer>
-	<p>donec &bull; sapien &bull; erat &bull; lorem &bull; ipsum</p>
+	<p><a href="company.php">company</a> &bull; <a href="blog.php">blog </a>&bull; <a href="clients.php">clients </a>
+	&bull; <a href="case_studies.php">case studies </a>&bull; <a href="products.php">products </a>&bull;
+	<a href="services.php">services</a></p>
 	<p>Serial Metrics | 1 Market Street, San Francisco | 555-555-5555</p>
 </footer>
