e05f93d6df5ec5544dc9651d33046395cae7b743
Bjorn Cooley <bjorncooley@gmail.com>
2014-03-06 15:59:55 +0800

    Update company page with new content

--- company.php
+++ company.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Clients</title>
+	<title>Company</title>
 	<?php include('includes/head_links.php'); ?>
 	<link rel="stylesheet" type="text/css" href="styles/company.css">
 </head>
@@ -10,51 +10,34 @@
 	<div class="main-content">
 		<h1>About Serial Metrics</h1>
 		<p>
-			Aliquam ac orci vel mi egestas molestie. Sed rhoncus, turpis ac bibendum 
-			condimentum, ante neque consequat sapien, posuere ornare quam massa at 
-			magna. Maecenas vitae vulputate ipsum. Proin quis velit non ante congue 
-			iaculis. Aenean vel lacus ac odio laoreet imperdiet ut quis tellus. Sed 
-			ut mattis odio, sed laoreet tortor. Nam fringilla viverra lorem a elementum. 
-			Donec ornare lectus non massa mollis, quis porta lectus gravida. Integer 
-			porta quam non lobortis elementum. Class aptent taciti sociosqu ad litora 
-			torquent per conubia nostra, per inceptos himenaeos. Vestibulum et ornare ante. 
-			Sed ullamcorper rutrum sem nec ultricies. Suspendisse nec elementum elit, at 
-			dictum massa. Nam consectetur consectetur mattis.
+			Founded in 2010, Serial Metrics delivers data science solutions to 
+			both enterprise companies and newly-minted start-ups.  
+		</p>
+		<p>
+			Serial Metrics leads in delivering data science solutions that help 
+			companies effectively mine their data to build algorithmic, 
+			server-side applications that solve a variety of common business problems. 
+			Companies leverage Serial Metrics APIs to offer their customers innovative 
+			solutions such as dynamic pricing, highly-targeted personalized recommendations, 
+			curated content, etc. With Serial Metrics, companies no longer need to address 
+			their data science issues with traditional in-house means to derive solutions 
+			from scratch; leveraging Serial Metrics&rdquo; suite of machine learning modules 
+			solves the most complex data science problems facing today&rdquo;s companies.
 		</p>
 	</div>
 	<img src="images/client_page/sf_bay.jpg">
 	<div class="main-content">
-		<p>
-			Aliquam ac orci vel mi egestas molestie. Sed rhoncus, turpis ac bibendum 
-			condimentum, ante neque consequat sapien, posuere ornare quam massa at magna. 
-			Maecenas vitae vulputate ipsum. Proin quis velit non ante congue iaculis. Aenean 
-			vel lacus ac odio laoreet imperdiet ut quis tellus. Sed ut mattis odio, sed laoreet 
-			tortor. Nam fringilla viverra lorem a elementum. Donec ornare lectus non massa mollis, 
-			quis porta lectus gravida. Integer porta quam non lobortis elementum. Class aptent 
-			taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. 
-			Vestibulum et ornare ante. Sed ullamcorper rutrum sem nec ultricies. Suspendisse nec 
-			elementum elit, at dictum massa. Nam consectetur consectetur mattis.
-		</p>
-		<h1>Laoreet</h1>
-		<p>
-			Aliquam ac orci vel mi egestas molestie. Sed rhoncus, turpis ac bibendum 
-			condimentum, ante neque consequat sapien, posuere ornare quam massa at 
-			magna. Maecenas vitae vulputate ipsum. Proin quis velit non ante congue 
-			iaculis. Aenean vel lacus ac odio laoreet imperdiet ut quis tellus. Sed 
-			ut mattis odio, sed laoreet tortor. Nam fringilla viverra lorem a elementum. 
-			Donec ornare lectus non massa mollis, quis porta lectus gravida. Integer 
-			porta quam non lobortis elementum. Class aptent taciti sociosqu ad litora 
-			torquent per conubia nostra, per inceptos himenaeos. Vestibulum et ornare ante. 
-			Sed ullamcorper rutrum sem nec ultricies. Suspendisse nec elementum elit, at 
-			dictum massa. Nam consectetur consectetur mattis.
-		</p>
-		<p>
-			Donec ornare lectus non massa mollis, 
-			quis porta lectus gravida. Integer porta quam non lobortis elementum. Class aptent 
-			taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. 
-			Vestibulum et ornare ante. Sed ullamcorper rutrum sem nec ultricies. Suspendisse nec 
-			elementum elit, at dictum massa. Nam consectetur consectetur mattis.
-		</p>
+		<h1>Our Team</h1>
+		<ul>
+			<li><strong>Amir Behbani</strong> - Founder / Algorithms</li>
+			<li><strong>Emiko Higashi</strong> - Partnerships</li>
+			<li><strong>Bjorn Cooley</strong> - Marketing</li>
+			<li><strong>Igor Savic</strong> - Engineering</li>
+			<li><strong>Jason Yu</strong> - Research</li>
+			<li><strong>Ben Smucker</strong> - Sales</li>
+			<li><strong>Imran Khalik</strong> - Legal</li>
+			<li><strong>Katherin Yi </strong> - Data Science</li>
+		</ul>
 	</div>
 	<?php include('includes/footer.php'); ?>
 </body>
