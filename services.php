54c052452137b0d55d88805cc3e813ca4a11c5eb
Bjorn Cooley <bjorncooley@gmail.com>
2014-03-07 10:43:03 +0800

    Add services page content + styles

--- services.php
+++ services.php
@@ -1,61 +1,54 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Clients</title>
+	<title>Services</title>
 	<?php include('includes/head_links.php'); ?>
-	<link rel="stylesheet" type="text/css" href="styles/clients.css">
+	<link rel="stylesheet" type="text/css" href="styles/services.css">
 </head>
 <body class="inside-page">
 	<?php include('includes/main_nav.php'); ?>
 	<div class="main-content">
 		<img src="images/services_page/graph.png">
 		<h1>Services</h1>
-		<p>
-			Aliquam ac orci vel mi egestas molestie. Sed rhoncus, turpis ac bibendum 
-			condimentum, ante neque consequat sapien, posuere ornare quam massa at 
-			magna. Maecenas vitae vulputate ipsum. Proin quis velit non ante congue 
-			iaculis. Aenean vel lacus ac odio laoreet imperdiet ut quis tellus. Sed 
-			ut mattis odio, sed laoreet tortor. Nam fringilla viverra lorem a elementum. 
-			Donec ornare lectus non massa mollis, quis porta lectus gravida. Integer 
-			porta quam non lobortis elementum. Class aptent taciti sociosqu ad litora 
-			torquent per conubia nostra, per inceptos himenaeos. Vestibulum et ornare ante. 
-			Sed ullamcorper rutrum sem nec ultricies. Suspendisse nec elementum elit, at 
-			dictum massa. Nam consectetur consectetur mattis.
-		</p>
-	</div>
-	<img src="images/client_page/sf_bay.jpg">
-	<div class="main-content">
-		<p>
-			Aliquam ac orci vel mi egestas molestie. Sed rhoncus, turpis ac bibendum 
-			condimentum, ante neque consequat sapien, posuere ornare quam massa at magna. 
-			Maecenas vitae vulputate ipsum. Proin quis velit non ante congue iaculis. Aenean 
-			vel lacus ac odio laoreet imperdiet ut quis tellus. Sed ut mattis odio, sed laoreet 
-			tortor. Nam fringilla viverra lorem a elementum. Donec ornare lectus non massa mollis, 
-			quis porta lectus gravida. Integer porta quam non lobortis elementum. Class aptent 
-			taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. 
-			Vestibulum et ornare ante. Sed ullamcorper rutrum sem nec ultricies. Suspendisse nec 
-			elementum elit, at dictum massa. Nam consectetur consectetur mattis.
-		</p>
-		<h1>Laoreet</h1>
-		<p>
-			Aliquam ac orci vel mi egestas molestie. Sed rhoncus, turpis ac bibendum 
-			condimentum, ante neque consequat sapien, posuere ornare quam massa at 
-			magna. Maecenas vitae vulputate ipsum. Proin quis velit non ante congue 
-			iaculis. Aenean vel lacus ac odio laoreet imperdiet ut quis tellus. Sed 
-			ut mattis odio, sed laoreet tortor. Nam fringilla viverra lorem a elementum. 
-			Donec ornare lectus non massa mollis, quis porta lectus gravida. Integer 
-			porta quam non lobortis elementum. Class aptent taciti sociosqu ad litora 
-			torquent per conubia nostra, per inceptos himenaeos. Vestibulum et ornare ante. 
-			Sed ullamcorper rutrum sem nec ultricies. Suspendisse nec elementum elit, at 
-			dictum massa. Nam consectetur consectetur mattis.
-		</p>
-		<p>
-			Donec ornare lectus non massa mollis, 
-			quis porta lectus gravida. Integer porta quam non lobortis elementum. Class aptent 
-			taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. 
-			Vestibulum et ornare ante. Sed ullamcorper rutrum sem nec ultricies. Suspendisse nec 
-			elementum elit, at dictum massa. Nam consectetur consectetur mattis.
-		</p>
+		<p>Our Experts work with you to ensure that you efficiently solve your 
+		Machine Learning challenges.  Our consulting services cover three basic areas:</p>
+
+		<h2>Custom Machine Learning</h2>
+
+		<h3>Remote Model</h3> 
+		<p>Your company provides us data, we build solutions - 
+		offsite (include the methodology section on the current site)</p>
+
+		<h3>Onsite Model</h3>
+		<p>We deploy staff, onsite, at your company to work with 
+		your teams.  This model works quite well for companies that might need 
+		'full-time', dedicated, data science resources, but don't want to take the hire staff in-house.</p>
+
+
+		<h2>Module Implementation</h2>
+		<p>Over the last five years we've built a suit of 
+		machine learning applications.  We refer to these applications as "modules." 
+		Often, Data Science solutions require thinking through some very challenging, 
+		mathematically rigorous, modeling problems. Companies might spend months just 
+		trying to model a seemingly unique business problem  - a processes that typically 
+		requires hiring a team of engineers to mine data, formulate a hypothesis, code a 
+		set of algorithms, and test for model accuracy.  Interestingly, rarely are these 
+		business problems truly unique.  Instead, we find that different verticals try 
+		solving the same problems day in day out.  For example gaming companies seem 
+		particularly interested in solving problems relating to attrition, whereas 
+		advertising companies seem perplexed by improving the conversion rates.</p>
+
+		<p>The benefit of the modules is that they prevent companies from having to reinvent the wheel.  
+		If a dynamic pricing algorithm exists, why not just use it? Similarly if the system exists that 
+		can predict churn, why not just leverage it. There's really very little point to reinvent the algorithm.</p> 
+
+		<p>The serial metrics modules eliminate the need to apply machine learning solutions 
+		to solve problems that need machine learning solutions. These modules only require 
+		you to plug in your data and will provide an answer. Super simple.</p>
+
+		<h2>Infrastructure</h2>
+		<h3>Orion Support</h3>
+
 	</div>
 	<?php include('includes/footer.php'); ?>
 </body>
