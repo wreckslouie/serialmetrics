ef46c5e1d781c20a54459d96b92092af45b1a177
Bjorn Cooley <bjorncooley@gmail.com>
2014-03-07 10:56:10 +0800

    Add services page content

--- clients.php
+++ clients.php
@@ -12,52 +12,32 @@
 		<h1>
 			Clients
 		</h1>
-		<p>
-			Aliquam ac orci vel mi egestas molestie. Sed rhoncus, turpis ac bibendum 
-			condimentum, ante neque consequat sapien, posuere ornare quam massa at 
-			magna. Maecenas vitae vulputate ipsum. Proin quis velit non ante congue 
-			iaculis. Aenean vel lacus ac odio laoreet imperdiet ut quis tellus. Sed 
-			ut mattis odio, sed laoreet tortor. Nam fringilla viverra lorem a elementum. 
-			Donec ornare lectus non massa mollis, quis porta lectus gravida. Integer 
-			porta quam non lobortis elementum. Class aptent taciti sociosqu ad litora 
-			torquent per conubia nostra, per inceptos himenaeos. Vestibulum et ornare ante. 
-			Sed ullamcorper rutrum sem nec ultricies. Suspendisse nec elementum elit, at 
-			dictum massa. Nam consectetur consectetur mattis.
-		</p>
-	</div>
-	<img src="images/client_page/sf_bay.jpg">
-	<div class="main-content">
-		<p>
-			Aliquam ac orci vel mi egestas molestie. Sed rhoncus, turpis ac bibendum 
-			condimentum, ante neque consequat sapien, posuere ornare quam massa at magna. 
-			Maecenas vitae vulputate ipsum. Proin quis velit non ante congue iaculis. Aenean 
-			vel lacus ac odio laoreet imperdiet ut quis tellus. Sed ut mattis odio, sed laoreet 
-			tortor. Nam fringilla viverra lorem a elementum. Donec ornare lectus non massa mollis, 
-			quis porta lectus gravida. Integer porta quam non lobortis elementum. Class aptent 
-			taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. 
-			Vestibulum et ornare ante. Sed ullamcorper rutrum sem nec ultricies. Suspendisse nec 
-			elementum elit, at dictum massa. Nam consectetur consectetur mattis.
-		</p>
-		<h1>Laoreet</h1>
-		<p>
-			Aliquam ac orci vel mi egestas molestie. Sed rhoncus, turpis ac bibendum 
-			condimentum, ante neque consequat sapien, posuere ornare quam massa at 
-			magna. Maecenas vitae vulputate ipsum. Proin quis velit non ante congue 
-			iaculis. Aenean vel lacus ac odio laoreet imperdiet ut quis tellus. Sed 
-			ut mattis odio, sed laoreet tortor. Nam fringilla viverra lorem a elementum. 
-			Donec ornare lectus non massa mollis, quis porta lectus gravida. Integer 
-			porta quam non lobortis elementum. Class aptent taciti sociosqu ad litora 
-			torquent per conubia nostra, per inceptos himenaeos. Vestibulum et ornare ante. 
-			Sed ullamcorper rutrum sem nec ultricies. Suspendisse nec elementum elit, at 
-			dictum massa. Nam consectetur consectetur mattis.
-		</p>
-		<p>
-			Donec ornare lectus non massa mollis, 
-			quis porta lectus gravida. Integer porta quam non lobortis elementum. Class aptent 
-			taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. 
-			Vestibulum et ornare ante. Sed ullamcorper rutrum sem nec ultricies. Suspendisse nec 
-			elementum elit, at dictum massa. Nam consectetur consectetur mattis.
-		</p>
+		
+		<a title="eventbrite-logo-e1315080753495" href="http://www.eventbrite.com/"><img class="attachment-full" title="" alt="eventbrite-logo-e1315080753495" src="http://serialmetrics.com/wp-content/uploads/2011/09/eventbrite-logo-e1315080753495.jpg" width="243" height="130" /></a>
+		<a title="AAA-logo" href="http://www.aaa.com/"><img class="attachment-full" title="" alt="AAA-logo" src="http://serialmetrics.com/wp-content/uploads/2011/09/AAA-logo.jpg" width="248" height="185" /></a>
+		<a title="WellsFargoLogo" href="https://www.wellsfargo.com/"><img class="attachment-full" title="" alt="WellsFargoLogo" src="http://serialmetrics.com/wp-content/uploads/2011/09/WellsFargoLogo.png" width="136" height="135" /></a>
+		&nbsp;
+
+		<a title="BACLogo" href="https://www.bankofamerica.com/"><img class="attachment-full" title="" alt="BACLogo" src="http://serialmetrics.com/wp-content/uploads/2011/09/BACLogo.png" width="258" height="113" /></a>
+		<a title="sycleBig" href="http://web.sycle.net/"><img class="attachment-full" title="" alt="sycleBig" src="http://serialmetrics.com/wp-content/uploads/2011/09/sycleBig.png" width="269" height="107" /></a>
+		&nbsp;
+
+		<a href="http://serialmetrics.com/project/swaylo/swaylo-fb/" rel="attachment wp-att-1004"><img class="alignright size-full wp-image-1004" alt="swaylo fb" src="http://serialmetrics.com/wp-content/uploads/2013/06/swaylo-fb.jpg" width="300" height="225" /></a>
+		<a title="pathto" href="https://www.path.to/"><img class="attachment-full" title="" alt="pathto" src="http://serialmetrics.com/wp-content/uploads/2011/09/pathto-e1371947172804.png" width="300" height="75" /></a>
+		&nbsp;
+
+		<a title="Harvard" href="http://www.hbs.edu/"><img class="attachment-full" title="" alt="Harvard" src="http://serialmetrics.com/wp-content/uploads/2011/09/Harvard.jpg" width="200" height="200" /></a>
+		<a title="Actiance-logo" href="http://www.actiance.com/"><img class="attachment-full" title="" alt="Actiance-logo" src="http://serialmetrics.com/wp-content/uploads/2011/09/Actiance-logo.jpg" width="288" height="130" /></a>
+		<a title="zipongo" href="http://www.zipongo.com/"><img class="attachment-full" title="" alt="zipongo" src="http://serialmetrics.com/wp-content/uploads/2011/09/zipongo-e1371946612882.jpg" width="250" height="101" /></a>
+		&nbsp;
+
+		<a title="ibm-logo" href="http://www.ibm.com/us/en/"><img class="attachment-full" title="" alt="ibm-logo" src="http://serialmetrics.com/wp-content/uploads/2011/09/ibm-logo.jpg" width="140" height="140" /></a>
+		<a title="Print" href="http://www.waypointhomes.com/"><img class="attachment-full" title="" alt="Print" src="http://serialmetrics.com/wp-content/uploads/2011/09/homes-logo.jpg" width="279" height="88" /></a>
+		<a title="Hang-logo" href="http://www.hang.co"><img class="attachment-full" title="" alt="Hang-logo" src="http://serialmetrics.com/wp-content/uploads/2011/09/Hang-logo.jpg" width="288" height="130" /></a>
+		<a title="topicalogo" href="http://www.topica.com/"><img class="attachment-full" title="" alt="topicalogo" src="http://serialmetrics.com/wp-content/uploads/2011/09/topicalogo.png" width="385" height="65" /></a>
+		&nbsp;
+
+		</div>
 	</div>
 	<?php include('includes/footer.php'); ?>
 </body>
