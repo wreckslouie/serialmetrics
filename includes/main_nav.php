5eba0907efd711e52770abc19617a441413dbecf
Bjorn Cooley <bjorncooley@gmail.com>
2014-03-11 15:43:38 +0700

    Update main nav with orion and module pages, adjust styling

--- includes/main_nav.php
+++ includes/main_nav.php
@@ -2,10 +2,11 @@
 	<a href="../serialmetrics.com" class="drop-down">Serial Metrics</a>
 	<ul>
 		<a href="company.php"><li>Company</li></a>
-		<a href="blog.php"><li>Blog</li></a>
+		<a href="module_products.php"><li>Module Products</li></a>
+		<a href="services.php"><li>Services</li></a>
+		<a href="orion.php"><li>Orion</li></a>
 		<a href="clients.php"><li>Clients</li></a>
 		<a href="case_studies.php"><li>Case Studies</li></a>
-		<a href="products.php"><li>Products</li></a>
-		<a href="services.php"><li>Services</li></a>
+		<a href="blog.php"><li>Blog</li></a>
 	</ul>
 </nav>
