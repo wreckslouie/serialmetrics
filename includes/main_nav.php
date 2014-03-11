c45171cf8cbddfa1bf20d88200451f4f7fb9f663
Bjorn Cooley <bjorncooley@gmail.com>
2014-03-11 17:10:39 +0700

    Update order of pages on main nav

--- includes/main_nav.php
+++ includes/main_nav.php
@@ -2,8 +2,8 @@
 	<a href="../serialmetrics.com" class="drop-down">Serial Metrics</a>
 	<ul>
 		<a href="company.php"><li>Company</li></a>
-		<a href="module_products.php"><li>Module Products</li></a>
 		<a href="services.php"><li>Services</li></a>
+		<a href="module_products.php"><li>Modules</li></a>
 		<a href="orion.php"><li>Orion</li></a>
 		<a href="clients.php"><li>Clients</li></a>
 		<a href="case_studies.php"><li>Case Studies</li></a>
