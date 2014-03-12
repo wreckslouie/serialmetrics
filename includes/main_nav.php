d413cd24dac652de7dbcbe3597bda433dc29d3c7
Bjorn Cooley <bjorncooley@gmail.com>
2014-03-12 16:28:17 +0700

    main_nav.php: update 'module_products.php' link to 'modules.php'

--- includes/main_nav.php
+++ includes/main_nav.php
@@ -3,7 +3,7 @@
 	<ul>
 		<a href="company.php"><li>Company</li></a>
 		<a href="services.php"><li>Services</li></a>
-		<a href="module_products.php"><li>Modules</li></a>
+		<a href="modules.php"><li>Modules</li></a>
 		<a href="orion.php"><li>Orion</li></a>
 		<a href="clients.php"><li>Clients</li></a>
 		<a href="case_studies.php"><li>Case Studies</li></a>
