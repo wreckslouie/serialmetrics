d58ba884adf08fa26c278216b99b4299a201cd56
Bjorn Cooley <bjorncooley@gmail.com>
2014-03-13 09:00:21 +0700

    modules.js: add drop-down functions for module anchor links

--- js/modules.js
+++ js/modules.js
@@ -1,4 +1,32 @@
 $(function(){
+	//Check if arriving at anchor link, drop down text if so
+	var query_string=$(location).attr('hash');
+	switch(query_string){
+		case '#matching-algorithms':
+			$('#matching-algorithms .module-content').slideDown(300);
+			break;
+		case '#dynamic-pricing':
+			$('#dynamic-pricing .module-content').slideDown(300);
+			break;
+		case '#recommendation-systems':
+			$('#recommendation-systems .module-content').slideDown(300);
+			break;
+		case '#text-classification':
+			$('#text-classification .module-content').slideDown(300);
+			break;
+		case '#intelligent-promotions':
+			$('#intelligent-promotions .module-content').slideDown(300);
+			break;
+		case '#scoring-systems':
+			$('#scoring-systems .module-content').slideDown(300);
+			break;
+		case '#geo-inference':
+			$('#geo-inference .module-content').slideDown(300);
+			break;
+		default:
+			break;
+	}
+
 	$('.module-heading').click(function(e){
 		var sibling_content=$(this).siblings('.module-content');
 		if(sibling_content.css('display')=='none'){
