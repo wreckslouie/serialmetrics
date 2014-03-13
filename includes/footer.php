84027730958eff55e9171169927c5b5b94f2c465
Bjorn Cooley <bjorncooley@gmail.com>
2014-03-13 18:26:04 +0700

    footer.php: update menu items

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
 <footer>
-	<p><a href="company.php">company</a> &bull; <a href="blog.php">blog </a>&bull; <a href="clients.php">clients </a>
-	&bull; <a href="case_studies.php">case studies </a>&bull; <a href="products.php">products </a>&bull;
-	<a href="services.php">services</a></p>
+	<p><a href="company.php">company</a> &bull; <a href="services.php">services </a>&bull; <a href="modules.php">modules </a>
+	&bull; <a href="orion.php">orion </a>&bull; <a href="clients.php">clients </a>&bull;
+	<a href="case_studies.php">case studies</a> &bull; <a href="blog.php">blog </a></p>
 	<p>Serial Metrics | 1 Market Street, San Francisco | <a href="tel:4085063000">408.506.3000</a></p>
 </footer>
