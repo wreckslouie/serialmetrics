2e1f3ac2983703edb590caf6a6e81e9a1ecc7e01
Bjorn Cooley <bjorncooley@gmail.com>
2014-03-16 22:54:14 +0800

    Update logos on client page

--- clients.php
+++ clients.php
@@ -15,17 +15,17 @@
 
 		<a href="http://www.aaa.com/"><img alt="AAA logo" src="images/client_page/aaa.png" /></a>
 		<a href="http://www.actiance.com/"><img alt="Actiance logo" src="images/client_page/actiance.png" /></a>
-		<a href="http://www.eventbrite.com/"><img alt="Eventbrite logo" src="images/client_page/eventbrite.jpg" /></a>
-		<a href="https://www.wellsfargo.com/"><img alt="Wells Fargo logo" src="images/client_page/wells-fargo.png" /></a>
 		<a href="https://www.bankofamerica.com/"><img alt="Bank of America logo" src="images/client_page/bank-of-america.png" /></a>
+		<a href="http://www.eventbrite.com/"><img alt="Eventbrite logo" src="images/client_page/eventbrite.png" /></a>
+		<a href="http://www.hang.co"><img alt="Hang logo" src="images/client_page/hang.png" /></a>
+		<a href="http://www.hbs.edu/"><img alt="Harvard Business School logo" src="images/client_page/harvard-business.png" /></a>
+		<a href="https://www.wellsfargo.com/"><img alt="Wells Fargo logo" src="images/client_page/wells-fargo.png" /></a>
 		<a href="http://web.sycle.net/"><img alt="sycleBig logo" src="images/client_page/sycle.png" /></a>
-		<a href="http://serialmetrics.com/project/swaylo/swaylo-fb/"><img alt="Swaylo Facebook logo" src="images/client_page/swaylo.jpg" /></a>
+		<a href="http://serialmetrics.com/project/swaylo/swaylo-fb/"><img alt="Swaylo Facebook logo" src="images/client_page/swaylo.png" /></a>
 		<a href="https://www.path.to/"><img alt="Pathto logo" src="images/client_page/path-to.png" /></a>
-		<a href="http://www.hbs.edu/"><img alt="Harvard Business School logo" src="images/client_page/harvard-business.jpg" /></a>
-		<a href="http://www.zipongo.com/"><img alt="Zipongo logo" src="himages/client_page/zipongo.jpg" /></a>
-		<a href="http://www.ibm.com/us/en/"><img alt="IBM logo" src="images/client_page/ibm.jpg" /></a>
-		<a href="http://www.waypointhomes.com/"><img alt="Waypoint logo" src="images/client_page/waypoint.jpg" /></a>
-		<a href="http://www.hang.co"><img alt="Hang logo" src="images/client_page/hang.jpg" /></a>
+		<a href="http://www.zipongo.com/"><img alt="Zipongo logo" src="images/client_page/zipongo.png" /></a>
+		<a href="http://www.ibm.com/us/en/"><img alt="IBM logo" src="images/client_page/ibm.png" /></a>
+		<a href="http://www.waypointhomes.com/"><img alt="Waypoint logo" src="images/client_page/waypoint.png" /></a>
 		<a href="http://www.topica.com/"><img alt="Topica logo" src="images/client_page/topica.png" /></a>
 		&nbsp;
 
