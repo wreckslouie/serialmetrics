e7b3870b5762f97ac3c8cacfa3ed7e639d176c85
Earvin Gemenez <earvin@ingenuity-tech.com>
2014-11-26 21:13:53 +0800

    added modules pages

---
+++ scoring_systems.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Scoring Systems</title>
+    <?php include('includes/head_links.php'); ?>
+    <link rel="stylesheet" type="text/css" href="styles/modules.css">
+    <script type="text/javascript" src="js/modules.js"></script>
+</head>
+<body class="inside-page">
+
+    <?php include('includes/main_nav.php'); ?>
+
+
+    <div id="main-content">
+        <img src="images/home_page/abacus-icon.png">
+        <h1>Scoring Systems  -  Identify Quality, Quantifiably</h1>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+        ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+        non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+    </div>
+
+
+    <?php include('includes/footer.php'); ?>
+
+</body>
+</html>
