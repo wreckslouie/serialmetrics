2a55d708e44ba2b89f7556979fc67128730af9d5
Earvin Gemenez <earvin@ingenuity-tech.com>
2014-12-18 08:02:43 +0800

    text updates

--- intelligent_promotions.php
+++ intelligent_promotions.php
@@ -13,15 +13,16 @@
 
     <div id="main-content">
         <img class="ico-modules" src="images/home_page/relations-icon.png">
-        <h1>Intelligent Promotions -  The Right Deal at the Right Time</h1>
+        <h1>Intelligent Promotions</h1>
 
-        <p>Engages users that are less likely to purchase from your site with deals, coupons, discounts, or other
-        offers before they leave your site without making a purchase.  Instantly identify the users that will
-        buy from your store, know when they will buy,  and even know the exact purchase amount.  Indiscriminately
-        offering deals to site visitors - in the form of discounts or coupons - is very often revenue sub-optimal.
-        If we can simply identify users that are least likely to purchase, but most likely to respond to a promotion,
-        and target them exclusively, and instantly upon their visiting your site, then we can maximize returns on
-        promotional campaigns.</p>
+        <p>Promotions and discounts are among the most powerful tools to drive incremental revenues, but they are often
+        costly to implement, and require a watchful eye to ensure a desired response. Serial Metrics offers a data-driven
+        approach that identifies the optimal group of customers to target for a promotions - those least likely to make a
+        purchase absent a promotion, and most likely to be influenced by a promotion.  By showing promotions to visitors who
+        wouldn&rsquo;t otherwise make a purchase, you can avoid eroding existing sales and limiting the promotion&rsquo;s brand
+        impact, boosting both revenue and margin.</p>
+
+        <p>The Orion Promotional API integrates onto your site for a seamless experience that drives revenues and protects your brand.</p>
 
         <div class="content">
             <h1>HOW TO WORK WITH SERIAL METRICS</h1>
@@ -30,13 +31,13 @@
             <p class="description">Integration requires loading our Java SDK, which scans your databases for relevant, and predicatively valuable features.</p>
 
             <h2>Learning About Users</h2>
-            <p class="description">Our system will then develop an initial model from the column variables automatically mined from your database.</p>
+            <p class="description">Our system will then develop an initial model from the column variables automatically mined from your database. We call this, automated feature selection.</p>
 
             <h2>Fine-tuning Predictions</h2>
             <p class="description">To test the efficacy of the predictive models, Serial Metrics fine tunes model parameters to reduce type-1 and type-2 statistical errors.</p>
 
             <h2>Deploying Results</h2>
-            <p class="description">The final model deploys as an API which monitors your database for new users, or changes to current user&rsquo;s tastes/behavior, and writes scores to a designated &lsquo;propensity&rsquo; table.</p>
+            <p class="description">The final model deploys as an API which monitors your database for new users, or changes to current user&rsquo;s tastes/behavior, and writes scores to a designated &lsquo;propensity&rsquo; table. This table is continuously updated as parameters that affect potential matches change. The results are always up-to-date.</p>
 
             <p>All you need to do is query the propensity table for matches.</p>
         </div>
--- matching_algorithms.php
+++ matching_algorithms.php
@@ -13,7 +13,7 @@
 
     <div id="main-content">
         <img class="ico-modules" src="images/home_page/share-icon.png">
-        <h1>Matching Algorithms -  The Perfect Job, The Perfect Partner</h1>
+        <h1>Matching Algorithms</h1>
 
         <p>Anyone who has ever dated or looked for a job is familiar with the matchmaking process.  It&rsquo;s often
         a costly, and time-consuming process to find the right &lsquo;one.&rsquo;  Various platforms attempt to match users
@@ -34,13 +34,13 @@
             <p class="description">Integration requires loading our Java SDK, which scans your databases for relevant, and predicatively valuable features.</p>
 
             <h2>Learning About Users</h2>
-            <p class="description">Our system will then develop an initial model from the column variables automatically mined from your database.</p>
+            <p class="description">Our system will then develop an initial model from the column variables automatically mined from your database. We call this, automated feature selection.</p>
 
             <h2>Fine-tuning Predictions</h2>
             <p class="description">To test the efficacy of the predictive models, Serial Metrics fine tunes model parameters to reduce type-1 and type-2 statistical errors.</p>
 
             <h2>Deploying Results</h2>
-            <p class="description">The final model deploys as an API which monitors your database for new users, or changes to current user&rsquo;s tastes/behavior, and writes scores to a designated &lsquo;propensity&rsquo; table.</p>
+            <p class="description">The final model deploys as an API which monitors your database for new users, or changes to current user&rsquo;s tastes/behavior, and writes scores to a designated &lsquo;propensity&rsquo; table. This table is continuously updated as parameters that affect potential matches change. The results are always up-to-date.</p>
 
             <p>All you need to do is query the propensity table for matches.</p>
         </div>
